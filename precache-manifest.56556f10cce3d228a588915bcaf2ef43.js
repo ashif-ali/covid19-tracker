1bc691f5ce48622ad762ec736d8998bba97543db
Ashif <ashifali2327@outlook.com>
2020-04-10 01:04:16 +0530

    Updates

---
+++ precache-manifest.56556f10cce3d228a588915bcaf2ef43.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac665abfb3747604b9ae20eb6df5b302",
+    "url": "/covid19-tracker/index.html"
+  },
+  {
+    "revision": "d862e18c8ce9c922a2aa",
+    "url": "/covid19-tracker/static/css/main.2b112300.chunk.css"
+  },
+  {
+    "revision": "59fc040d0a24b9010b79",
+    "url": "/covid19-tracker/static/js/2.74929734.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid19-tracker/static/js/2.74929734.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d862e18c8ce9c922a2aa",
+    "url": "/covid19-tracker/static/js/main.698c94f8.chunk.js"
+  },
+  {
+    "revision": "49fe9ade3c0196828749",
+    "url": "/covid19-tracker/static/js/runtime-main.407245ff.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid19-tracker/static/media/image.d7265326.png"
+  }
+]);
